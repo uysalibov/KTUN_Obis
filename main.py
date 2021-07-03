34d3945208e42a6df887a6eb1c6731cdbcf4faf1
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2021-07-03 13:03:02 +0300

    Minik bug fixleme

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--headless') # tarayıcının açılmasını istiyorsan bu satırı yorum satırı yap
 
-# Tarayıcı
-browser = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
 while True:
+    # Tarayıcı
+    browser = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
     flag = 0
     while(flag == 0):
         # Okul sayfasına git
